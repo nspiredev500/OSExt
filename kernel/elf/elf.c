9c0c8b39f63cd066386c71e571886b044ac57f09
nspiredev500 <57017842+nspiredev500@users.noreply.github.com>
2020-04-27 21:10:54 +0200

    started elf parsing

---
+++ kernel/elf/elf.c
@@ -0,0 +1,68 @@
+#include "../kernel.h"
+
+
+
+
+
+
+// returns true if it's a valid ELF file
+bool elf_check_header(struct elf_header *h)
+{
+	if (h->magic != 0x7f454c46) // 0x7f ELF
+	{
+		return false;
+	}
+	if (h->wordwidth != 1) // must be 32 bit
+	{
+		return false;
+	}
+	if (h->endianness != 1) // must be little endian
+	{
+		return false;
+	}
+	if (h->instruction_set != 0x28) // arm
+	{
+		return false;
+	}
+	return true;
+}
+
+
+
+void elf_read_header(NUC_FILE * f,struct elf_header *h)
+{
+	char[52] header; // the ELF header is 52 bytes for 32 bit
+	nuc_fread(header,1,52,f);
+	k_memcpy(&h->magic,header,4);
+	h->wordwidth = header[4];
+	h->endianness = header[5];
+	h->elf_h_version = header[6];
+	h->abi = header[7];
+	k_memcpy(&h->type,header+0x10,2);
+	k_memcpy(&h->instruction_set,header+0x12,2);
+	k_memcpy(&h->elf_version,header+0x14,4);
+	k_memcpy(&h->entry,header+0x18,4);
+	k_memcpy(&h->prog_table,header+0x1c,4);
+	k_memcpy(&h->sect_table,header+0x20,4);
+	k_memcpy(&h->header_size,header+0x28,2);
+	k_memcpy(&h->prog_size,header+0x2a,2);
+	k_memcpy(&h->prog_count,header+0x2c,2);
+	k_memcpy(&h->sect_size,header+0x2e,2);
+	k_memcpy(&h->sect_count,header+0x30,2);
+	k_memcpy(&h->sect_index,header+0x32,2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ kernel/elf/elf.h
@@ -0,0 +1,48 @@
+#ifndef ELF_H
+#define ELF_H
+
+
+
+struct elf_header {
+	uint32_t magic;
+	uint8_t wordwidth; // 1 = 32 bit, 2 = 64 bit
+	uint8_t endianness; // 1 = little endian, 2 = big endian
+	uint8_t elf_h_version; // elf header version
+	uint8_t abi;
+	uint16_t type; // 1 = relocatable, 2 = executable, 3 = shared, 4 = core
+	uint16_t instruction_set;
+	uint32_t elf_version;
+	uint32_t entry; // entry point position
+	uint32_t prog_table; // program header table
+	uint32_t sect_table;
+	uint16_t header_size;
+	uint16_t prog_size;
+	uint16_t prog_count;
+	uint16_t sect_size;
+	uint16_t sect_count;
+	uint16_t sect_index; // Index in section header table with the section names
+};
+
+
+
+
+// returns true if it's a valid ELF file
+bool elf_check_header(struct elf_header *h);
+
+
+
+void elf_read_header(NUC_FILE * f,struct elf_header *h);
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
